5989988550ce1bd7cd4bb8787ad825f166c025df
Sai Katterishetty <rao.saikrishna@gmail.com>
2025-12-12 12:08:26 -0600

    Fix typo in SYSTEM_DB_FILE definition in sysmodel.h

--- src/sysmodel.h
+++ src/sysmodel.h
@@ -9,7 +9,7 @@
 #define MAX_CPU_OS_LENGTH 32
 #define SYSTEM_ID_MIN 7000
 #define SYSTEM_ID_MAX 8000
-#define SYSTEM_DB_FILE system.db "
+#define SYSTEM_DB_FILE system.db
 
 struct SystemInventoryHeader
 {
